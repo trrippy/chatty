c8755c7d91d062dd0e9d8bf5ebdeeccbd1d739b7
trrippy <wes.wong@hotmail.com>
2017-02-07 21:06:47 +0000

    handles keypress enter and invokes onSend giving it the value of user input

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {value: ''};
+  }
+  handleChange(e) {
+    if (e.key === 'Enter') {
+      this.state.value = e.target.value;
+      console.log(e.target.value)
+      // invokes function in app.jsx
+      this.props.onSend(e.target.value);
+    }
+  }
+
   render() {
+    const value = this.state.value;
     return (
+
       <footer className="chatbar">
         <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+          onKeyPress={this.handleChange} />
       </footer>
     )
   }
