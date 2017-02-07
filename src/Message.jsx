fd479489cfcae0e5bab856037c74e7b2c3efc504
trrippy <wes.wong@hotmail.com>
2017-02-07 19:56:15 +0000

    cleaned up a bit

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
 
 class Message extends Component {
-  constructor(props) {
-    super(props);
-      console.log('now with this', this.props);
-  }
+
   render() {
     return (
       <div>
