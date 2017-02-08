08414aff41546a63b2c28d9c8cc73c723cecf6d1
trrippy <wes.wong@hotmail.com>
2017-02-08 21:40:26 +0000

    added conditional to messages map to display notification instead

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -4,11 +4,31 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      currentUser: {name: 'Anonymous'},
+    };
+
+  }
+
+
+
   render() {
     return (
       <main className="messages">
         {this.props.messages.map(item => {
-          return <Message username={item.username} content={item.content} key={item.id} />
+          console.log(item.type);
+          if (item.type === 'notification') {
+            return <div className="message system">{item.message}</div>
+          } else {
+            return (
+              <Message
+              username={item.username}
+              content={item.content}
+              key={item.id} />
+            )
+          }
         })}
 
       </main>
